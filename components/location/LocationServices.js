c8f37cfbc90d5bf0429cfa4114933b99f679d526
sebhani <hani-111-222@hotmail.com>
2020-04-22 22:11:39 -0400

    Add getCurrentLocation function which is responsible for getting the users' location and store both longitude and latitude

--- components/location/LocationServices.js
+++ components/location/LocationServices.js
@@ -3,3 +3,15 @@ import i18n from 'i18n-js';
 import * as Permissions from 'expo-permissions';
 import * as ExpoLocation from 'expo-location';
 import { Alert, Linking } from 'react-native';
+
+async function getCurrentLocation(object) {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    const location = await ExpoLocation.getCurrentPositionAsync({});
+    object.setState({
+        region: {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+        }
+    }, () => { object.props.updateRegion(object.state.region); });
+}
