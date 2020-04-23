2e77f5b2afb5f124d9f350e5032b7eee516e8fde
sebhani <hani-111-222@hotmail.com>
2020-04-22 21:58:04 -0400

    Add LocationServices file which is going to be responsible for determining the current location functionality

---
+++ components/location/LocationServices.js
@@ -0,0 +1,5 @@
+/* istanbul ignore file */
+import i18n from 'i18n-js';
+import * as Permissions from 'expo-permissions';
+import * as ExpoLocation from 'expo-location';
+import { Alert, Linking } from 'react-native';
