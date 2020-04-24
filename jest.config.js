f6879c63e1ef2609e3d85cd68177b5f3f1069c75
Mohamed <mohhef@gmail.com>
2020-04-23 20:04:46 -0400

    empty

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Load setup-tests.js before test execution
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['<rootDir>setup-tests.js'],
-  transformIgnorePatterns: ['../../node_modules/(?!${@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView})'],
-  // ...
-};
